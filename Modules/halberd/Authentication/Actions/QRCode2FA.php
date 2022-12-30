2475d237b680ab444d2745cd648a559653656fa6
grimpirate <grimpirate_jrs@yahoo.com>
2022-12-30 12:27:09 -0500

    namespace modification

--- Modules/halberd/Authentication/Actions/QRCode2FA.php
+++ Modules/halberd/Authentication/Actions/QRCode2FA.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace CodeIgniter\Shield\Authentication\Actions;
+namespace Halberd\Authentication\Actions;
 
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RedirectResponse;
