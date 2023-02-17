4f070aa11403b466c3a034a77a1418e2a338d16a
grimpirate <grimpirate_jrs@yahoo.com>
2023-02-17 16:46:13 -0500

    Layout simplification

--- Modules/halberd/Views/qrcode_activate_show.php
+++ Modules/halberd/Views/qrcode_activate_show.php
@@ -1,38 +1,9 @@
-<?= $this->extend(config('Auth')->views['layout']) ?>
-
-<?= $this->section('title') ?><?= lang('QRAuth.qrcode2FATitle') ?> <?= $this->endSection() ?>
+<?= $this->extend(config('Auth')->views['qrcode_layout']) ?>
 
 <?= $this->section('main') ?>
-
-<div class="container d-flex justify-content-center p-5">
-    <div class="card col-12 col-md-5 shadow-sm">
-        <div class="card-body">
-            <h5 class="card-title mb-5"><?= lang('QRAuth.qrcode2FATitle') ?></h5>
-
-            <?php if (session('error')) : ?>
-                <div class="alert alert-danger"><?= session('error') ?></div>
-            <?php endif ?>
 
             <p><?= lang('QRAuth.qrcodeActivateBody') ?></p>
 
             <p><?= $qrcode ?></p>
-
-            <form action="<?= site_url('auth/a/verify') ?>" method="post">
-                <?= csrf_field() ?>
-
-                <!-- Code -->
-                <div class="mb-2">
-                    <input type="text" class="form-control" name="token" placeholder="000000" inputmode="numeric"
-                        pattern="[0-9]*" autocomplete="one-time-code" value="<?= old('token') ?>" required />
-                </div>
-
-                <div class="d-grid col-8 mx-auto m-3">
-                    <button type="submit" class="btn btn-primary btn-block"><?= lang('Auth.confirm') ?></button>
-                </div>
-
-            </form>
-        </div>
-    </div>
-</div>
 
 <?= $this->endSection() ?>
